69df221fac34427065277780d88acd51f58ff2c6
jameson owings <jowings90@gmail.com>
2018-04-03 16:56:56 -0400

    First Commit

---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsPopup : MonoBehaviour {
+
+    [SerializeField] private Slider speedSlider;
+    
+    public void Open() {
+        gameObject.SetActive(true);
+    }
+
+    public void Close() {
+        gameObject.SetActive(false);
+    }
+
+    public void OnSubmitName(string name) {
+        Debug.Log(name);
+    }
+
+    //public void OnSpeedValue(float speed) {
+       
+    //}
+
+    public void OnSpeedValue(float value) {
+        Messenger<float>.Broadcast(GameEvent.SPEED_CHANGED, speed);
+        Debug.Log("Speed: " + speed);
+    }
+	// Use this for initialization
+	void Start () {
+        speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    [SerializeField] private Text scorelabel;
+    [SerializeField] private SettingsPopup settingsPopup; 
+
+	// Use this for initialization
+	
+    private int _score;
+    
+    void Awake() {
+        Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void OnDestroy() {
+        Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void Start() {
+        _score += 0;
+        scorelabel.text = _score.ToString();
+
+        settingsPopup.Close();
+    }
+
+    private void OnEnemyHit() {
+        _score += 1;
+        scorelabel.text = _score.ToString();
+    }
+
+    public void OnOpenSettings() {
+        settingsPopup.Open();
+    }
+}
